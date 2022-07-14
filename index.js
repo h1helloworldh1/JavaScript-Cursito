b157bd6bfdade6d9bca4b08a5987aae2191ebbe6
cristian rodriguez <cristianrod94@hotmail.com>
2022-07-13 23:54:06 -0300

    1er entrega del proyecto final

--- index.js
+++ index.js
@@ -64,46 +64,97 @@
 
 
 
-class Car{
-    constructor(brand, model, year, price){
-        this.brand = brand;
-        this.model = model;
-        this.year = year
-        this.price = price;
-        this.sold = false;
-}
+// class Car{
+//     constructor(brand, model, year, price){
+//         this.brand = brand;
+//         this.model = model;
+//         this.year = year
+//         this.price = price;
+//         this.sold = false;
+// }
 
-    sell(){
-        this.sold = true;
+//     sell(){
+//         this.sold = true;
+//     }
+// }
+
+// let cars = [];
+
+// cars.push(new Car("Toyota", "Supra", 1999, 28000));
+// cars.push(new Car("Mazda", "Miata Mx", 1998, 21000));
+// cars.push(new Car("Nissan", "GT-R Skyline", 1999, 48000));
+// cars.push(new Car("Subaru", "Impreza", 2003, 34000))
+
+// for(const product of cars){
+//     if(product.brand === "Mazda"){
+//         product.sell()
+//     }
+// }
+
+// console.log(cars);
+
+
+// const allCars = ["Toyota", "Mazda", "Nissan", "Subaru"];
+
+// const soldCars = (brand) => {
+//     let index = allCars.indexOf(brand);
+
+//     if(index != -1){
+//         allCars.splice(index, 1);
+//     }
+
+//     console.log(allCars)
+// }
+
+// soldCars("Mazda")
+
+// function operaciones(operacion){
+//     if(operacion === "suma"){
+//         return (x,y) => x + y;
+//     }
+    
+//     if(operacion === "resta"){
+//         return (x, y) => x - y;
+//     }
+//     }
+//     let compra = operaciones("suma");
+    
+//     console.log(compra(1,2));
+
+
+/* Array - Lista de Coches en venta */
+const cars = [
+    {brand: "Toyota", model:"Supra", year:1999, price: 28000},
+    {brand: "Mazda", model:"Miata Mx", year:1998, price: 21000},
+    {brand: "Nissan", model:"GT-R Skyline", year:1999, price: 48000},
+    {brand: "Subaru", model:"Impreza", year:2003, price: 34000},
+];
+
+/* Recorrido - Busqueda de coches */
+function showMeCar(cars, fast){
+    for(const car of cars){
+        fast(car)
     }
 }
+showMeCar(cars,console.log);
 
-let cars = [];
+/* Abrir live server, abrir consola de navegador. 
+Busqueda por marca de stock, ingresando en el prompt la marca con letra inicial
+mayuscula.*/
 
-cars.push(new Car("Toyota", "Supra", 1999, 28000));
-cars.push(new Car("Mazda", "Miata Mx", 1998, 21000));
-cars.push(new Car("Nissan", "GT-R Skyline", 1999, 48000));
-cars.push(new Car("Subaru", "Impreza", 2003, 34000))
-
-for(const product of cars){
-    if(product.brand === "Mazda"){
-        product.sell()
-    }
+let searchCar = prompt("Search Cars")
+const search = cars.find((myCar) => myCar.brand === searchCar)
+if (searchCar === "Mazda"){
+    document.write(`${searchCar} Stock - Miata Mx`);
+}
+if(searchCar === "Toyota"){
+    document.write(`${searchCar} Stock - Supra`);
+}
+if(searchCar === "Nissan"){
+    document.write(`${searchCar} Stock - GT-R Skyline`);
+}
+if(searchCar === "Subaru"){
+    document.write(`${searchCar} Stock - Impreza`);
 }
 
-console.log(cars);
-
-
-const allCars = ["Toyota", "Mazda", "Nissan", "Subaru"];
-
-const soldCars = (brand) => {
-    let index = allCars.indexOf(brand);
-
-    if(index != -1){
-        allCars.splice(index, 1);
-    }
-
-    console.log(allCars)
-}
-
-soldCars("Mazda")
+console.log(search)
